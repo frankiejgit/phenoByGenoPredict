e2c3ec8f269ed8d9d4970a1516f615ae35fc5914
frankiejgit <frankiejgram@gmail.com>
2023-06-21 00:59:09 -0400

    integrating module 1

--- src/multi_omics_platform/1_data_load.R
+++ src/multi_omics_platform/1_data_load.R
@@ -1,9 +1,10 @@
 ### 1 - Data Load ###
 
-function loadData(marker.path, phenos.path) {
+loadData <- function(marker.path, phenos.path) {
+
   # Load the covariate matrix file
   if(grepl("\\.rda$", marker.path)) {
-    markers <- load(marker.path)
+    markers <- get(load(marker.path))
   } else if (grepl("\\.csv$", marker.path)) {
     markers <- read.csv(markers.path)
   } else {
@@ -12,36 +13,41 @@ function loadData(marker.path, phenos.path) {
   
   # Load the phenos/environment file
   if(grepl("\\.rda$", phenos.path)) {
-    phenos <- load(marker.path)
+    phenos <- get(load(phenos.path))
   } else if (grepl("\\.csv$", phenos.path)) {
     phenos <- read.csv(phenos.path)
   } else {
     stop("Invalid file format for covariate matrix file, Only CSV and RDA files are supported.")
   }
   
+  return(list(markers = markers, phenos = phenos))  
+}
+
+createNaNFiles <- function(phenos, markers, nan.freq) {
+  # Use args[1] for NaN threshold, # of strains with missing values to keep
+  nan.freq.i <- length(unique(phenos[["strain"]])) * nan.freq
+  NaNs <- colSums(is.na(markers))
+  
+  index.1 <- order(NaNs)
+  NaN.freq <- unique(NaNs[index.1])
+  percentage.NaNs <- NaN.freq * 100 / nrow(markers)
+  
+  # Write the NaN frequencies and percentages to separate CSV files
+  write.table(NaN.freq, file = '../../tmp/NaNs_freq.csv', sep = ',', row.names = FALSE, col.names = FALSE)
+  cat("Saved the NaN.freq file in the tmp directory\n")
+  
+  write.table(percentage.NaNs, file = '../../tmp/percentage_NaNs.csv', sep = ',', row.names = FALSE, col.names = FALSE)
+  cat("Saved the percentage.NaNs file in the tmp directory\n")
+  
+  # Subset the markers matrix based on a condition and save it as 'X'
+  index.2 <- NaNs <= nan.freq.i
+  no.NaNs.markers <- markers[, index.2]
+  write.csv(no.NaNs.markers, file = '../../tmp/no_nans_markers.csv', row.names = FALSE)
+  cat("Saved the no.NaNs.markers file in the tmp directory\n")
+  
 }
 
 
 
-# Get the molecular marker information 
-load('../../data/markers.rda')
-assign("markers", SNPs, envir = .GlobalEnv)
-rm(SNPs)
-phenos <- read.csv('../../data/phenos.csv')
 
-# Use args[1] for missing value threshold
-nan.freq.i <- length(unique(phenos$strain)) * nan.freq
-NaNs <- colSums(is.na(markers))
 
-index.1 <- order(NaNs)
-NaN.freq <- unique(NaNs[index.1])
-percentage.NaNs <- NaN.freq * 100 / nrow(markers)
-
-# Write the NaN frequencies and percentages to separate CSV files
-write.table(NaN.freq, file = '../../data/NaNs.freq.csv', sep = ',', row.names = FALSE, col.names = FALSE)
-write.table(percentage.NaNs, file = '../../data/percentage.NaNs.csv', sep = ',', row.names = FALSE, col.names = FALSE)
-
-# Subset the 'SNPs' matrix based on a condition and save it as 'X'
-index.2 <- NaNs <= nan.freq.i
-X <- markers[, index.2]
-write.csv(X, file = '../../data/X.csv', row.names = FALSE)
--- src/multi_omics_platform/main.R
+++ src/multi_omics_platform/main.R
@@ -1,107 +1,23 @@
+# Import packages
 library(data.table)
 
-### ARGUMENTS TO PASS ###
+# Import modules
+source("1_data_load.R")
+
+### USER ARGUMENTS ###
 
 args <- commandArgs(trailingOnly = TRUE)
-nan.freq <- 0.2 # 1 - NaN threshold frequency
+nan.freq <- 0.2                            # NaN threshold frequency
+col.env.id <- 11                           # Env ID in phenos file
+marker.path <- "../../data/SNPs.rda"       # Covariate matrix file
+phenos.path <- "../../data/phenos.csv"     # Phenotype/Environment file
 
-# This may be something created by the program
-col.env.id <- 11 # The environment ID in the phenos file
+### 1 - Data Load ###
+loaded.data <- loadData(marker.path, phenos.path)
 
-### 1 - Splitting SNPs ###
+markers <- loaded.data$markers
+phenos <- loaded.data$phenos
 
-# Get the molecular marker information 
-load('../../data/markers.rda')
-assign("markers", SNPs, envir = .GlobalEnv)
-rm(SNPs)
-phenos <- read.csv('../../data/phenos.csv')
 
-# Use args[1] for missing value threshold
-nan.freq.i <- length(unique(phenos$strain)) * nan.freq
-NaNs <- colSums(is.na(markers))
-
-index.1 <- order(NaNs)
-NaN.freq <- unique(NaNs[index.1])
-percentage.NaNs <- NaN.freq * 100 / nrow(markers)
-
-# Write the NaN frequencies and percentages to separate CSV files
-write.table(NaN.freq, file = '../../data/NaNs.freq.csv', sep = ',', row.names = FALSE, col.names = FALSE)
-write.table(percentage.NaNs, file = '../../data/percentage.NaNs.csv', sep = ',', row.names = FALSE, col.names = FALSE)
-
-# Subset the 'SNPs' matrix based on a condition and save it as 'X'
-index.2 <- NaNs <= nan.freq.i
-X <- markers[, index.2]
-write.csv(X, file = '../../data/X.csv', row.names = FALSE)
-
-### 2 - Create G/E matrices ###
-
-## E - Phenos ##
-
-# Check parameters 
-if(is.null(markers)) {
-  cat('Are you working with a grouping factor?
-       If not, please provide a path of the covariate matrix file')
-  
-  # Prompt the user for input
-  user_input <- readline("Do you want to continue without the matrix file? (y/n): ")
-  
-  # Check the user's response
-  if (tolower(user_input) != "n") {
-    stop("Please provide the covariate matrix file and rerun the pipeline.")
-  }
-}
-
-### Reads data and perform consistency checks
-cat('=2===> Reading Data '); cat('\n')
-
-if (is.null(markers)) {
-  
-  Y <- phenos
-  Y[, col.env.id] <- factor(Y[, col.env.id])
-  n <- length(levels(Y[, col.env.id]))
-  Z <- as.matrix(model.matrix(~Y[, col.env.id] - 1))
-  d <- colSums(Z)
-  V <- Z
-  
-  for (i in 1:ncol(Z)) {
-    V[, i] <- V[, i] / sqrt(d[i])
-  }
-  
-  EVD <- list(vectors = V, values = d)
-  G <- tcrossprod(Z)
-  
-  save(G, file = '../../output/G.rda')
-  save(EVD, file = '../../output/EVD.rda')
-  } else {
-  ## Reads covariates
-  n <- ncol(markers)
-  p <- length(colnames(markers))
-  
-  Y <- matrix(nrow = n, ncol = p, NA)
-  id.Y <- rep(NA, n)
-  
-  for (i in 1:n) {
-    if (!is.null(col.env.id)) { id.Y[i] <- markers[1] }
-    
-    ifelse(!is.null(co))
-    
-    ifelse(!is.null(colIDy) == FALSE, X[i, ] <- as.numeric(tmp), X[i, ] <- as.numeric(tmp[-1]))
-    print(i)
-  }
-  close(fileIn)
-  rownames(X) <- IDx
-  colnames(X) <- colNames
-  
-  if (weighting) {
-    weight <- scan(weight.file, skip = 1)
-  }
-  
-  S <- 0
-  
-  ## Imputing, centering, standardizing and weighting.
-  for (i in 1:ncol(X)) {
-    meanXi <- mean(X[, i], na.rm = TRUE)
-    X[, i] <- ifelse(is.na(X[, i]), meanXi, X[, i])  # Naive imputation
-    if (ctr) { X[, i] <- X[, i] - meanXi }  # Centering
-    if (std) { X[, i] <- X[, i] / sd(X[, i]) }  # Standard
-    
+# Create the NaNs csv files
+createNaNFiles(phenos, markers, nan.freq)
