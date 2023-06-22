34bdd4cdf3b9dc9688f9f04a48e198f34253c387
frankiejgit <frankiejgram@gmail.com>
2023-06-21 22:31:18 -0400

    adding full matrices code in main

--- src/multi_omics_platform/main.R
+++ src/multi_omics_platform/main.R
@@ -1,5 +1,7 @@
 # Import packages
 library(data.table)
+library(ggplot2)
+library(gridExtra)
 
 # Import modules
 source("1_data_load.R")
@@ -12,11 +14,19 @@ col.env.id <- 11                           # Env ID in phenos file
 marker.path <- "../../data/SNPs.rda"       # Covariate matrix file
 phenos.path <- "../../data/phenos.csv"     # Phenotype/Environment file
 
+ctr <- TRUE
+std <- TRUE
+weighting <- FALSE                      # Remove line
+prop.maf.j <- NULL                         # Remove line
+
+
 ### 1 - Data Load ###
 loaded.data <- loadData(phenos.path, marker.path)
 
 markers <- loaded.data$markers
 phenos <- loaded.data$phenos
+
+rm(loaded.data)
 
 # Create the NaNs csv files
 createNaNFiles(phenos, markers, nan.freq)  # These files are the Mod1 outputs
@@ -46,7 +56,8 @@ if (!is.null(col.env.id)) {
   env.IDs <- phenos[, col.env.id]
 }
 
-if (is.null(markers)) {
+markers_2 <- NULL
+if (is.null(markers_2)) {
   env.IDs <- factor(phenos[, col.env.id])
   n <- length(levels(env.IDs))
   Z <- as.matrix(model.matrix(~env.IDs - 1))
@@ -58,55 +69,91 @@ if (is.null(markers)) {
   }
   
   EVD <- list(vectors = V, values = d)
-  E.G <- tcrossprod(Z)
+  e.G <- tcrossprod(Z)
   
-  save(E.G, file = '../../output/e_G.rda')
+  save(e.G, file = '../../output/e_G.rda')
   save(EVD, file = '../../output/e_EVD.rda')
   
 } else {
   
   # Reads covariates
-  colNames <- colnames(markers)
-  n <- nrow(markers)
-  p <- length(colNames)
-  
-  ##### TODO Remove mm.matrix, it is identical to `markers` #####
-  
-  # Create empty matrix with same dimensions as `markers`
-  mm.matrix <- matrix(nrow=n, ncol=p, NA)
-  # List of NaN values equal to length as rows in `markers`
-  id.mm <- rep(NA, n)
-  
-  for (i in 1:n) {
-    if (!is.null(col.env.id)) {
-      tmp <- markers[i,]    # Look at each line at a time
-      id.mm[i] <- row.names(markers)[i]    # TODO Original code uses markers[1], verify
-      mm.matrix[i,] <- as.numeric(tmp)
-    }
-  }
-  
-  rownames(mm.matrix) <- id.mm
-  colnames(mm.matrix) <- colNames
-  
-  ###### END of TODO ######
+  mm.matrix <- markers
+  s <- 0
+  col.count <- ncol(mm.matrix)
   
   if (weighting) {
-    weight <- scan(weight.file, skip =1 )
+    weight <- scan(weight.file, skip =1 ) # TODO check with Dr. Jarquin about this
   }
   
-  # Imputing, centering, standarizing and weighting
-  S <- 0
-  for (i in 1:p) {
+  # Naive imputation
+  for (i in 1:col.count) {
     mean.i <- mean(mm.matrix[, i], na.rm=TRUE)
-    # Naive imputation
-    mm.matrix[,i] <- ifelse(is.na(mm.matrix(X[,i])), mean.i, mm.matrix[,i])
-    print(i)
-    S <- S + var(mm.matrix[,i])
+    mm.matrix[,i] <- ifelse(is.na(mm.matrix[,i]), mean.i, mm.matrix[,i])
+    s <- s + var(mm.matrix[,i])
   }
   
-  p2 <- colMeans(mm.matrix, na.rm=TRUE) / 2
-  p2 <- ifelse(p <= 0.5, p, 1-p)
+  p <- colMeans(mm.matrix, na.rm=TRUE) / 2
+  p <- ifelse(p <= 0.5, p, 1-p)
   
+  # Adjusting for proportion of MAF
+  if (!is.null(prop.maf.j)) {
+    maf.idx <- which( p >= prop.maf.j )
+    mm.matrix <- mm.matrix[, maf.idx]
+  }
+  
+  # Imputing, centering, standardizing and weighting
+  for (i in 1:col.count) {
+    mean.i <- mean(mm.matrix[,i], na.rm=TRUE)
+    if (ctr) { mm.matrix[,i] <- mm.matrix[,i] - mean.i }            # Centering
+    if (std) { mm.matrix[,i] <- mm.matrix[,i]/sd(mm.matrix[,i]) }   # Standardizing
+    if (weighting) { mm.matrix[,i] <- mm.matrix[,i] * weight[i] }   # Weighting
+  }
+  
+  e.G <- tcrossprod(mm.matrix) / s
+
+  # Check all IDs are in e.G
+  if(!is.null(col.env.id)){
+    stopifnot(all(env.IDs%in%rownames(e.G))) 
+    }
+  
+  if(!is.null(col.env.id)){
+    env.IDs <- factor(env.IDs, levels=rownames(e.G))
+    e.Z <- as.matrix(model.matrix(~env.IDs-1))
+    e.G <- tcrossprod(tcrossprod(e.Z,e.G),e.Z)
+  }
+  
+  EVD <- eigen(e.G)
+  rownames(EVD$vectors) <- rownames(e.G)
+  
+  save(E.G, file = '../../output/e_G.rda')
+  save(EVD, file = '../../output/e_EVD.rda')
   
 }
 
+plt1 <- ggplot(data = as.data.frame(EVD$vectors), aes(x = EVD$vectors[, 1], y = EVD$vectors[, 2])) +
+  geom_point() +
+  labs(title = "", xlab = "First Component", ylab = "Second Component")
+plt2 <- ggplot(data = as.data.frame(EVD$vectors), aes(x = EVD$vectors[, 1], y = EVD$vectors[, 3])) +
+  geom_point() +
+  labs(title = "", xlab = "First Component", ylab = "Third Component")
+plt3 <- ggplot(data = as.data.frame(EVD$vectors), aes(x = EVD$vectors[, 2], y = EVD$vectors[, 2])) +
+  geom_point() +
+  labs(title = "", xlab = "Second Component", ylab = "Third Component")
+
+plt4 <- ggplot(data = as.data.frame(EVD$values), aes(x = seq_along(EVD$values), y = EVD$values)) +
+  geom_line() +
+  labs(title = "Eigen-values", x = "Components", y = "Eigen-value") +
+  theme_minimal()
+
+plt5 <- ggplot(data = as.data.frame(EVD$values), aes(x = seq_along(EVD$values), y = cumsum(EVD$values) / sum(EVD$values))) +
+  geom_line() +
+  labs(title = "Cumulative Variance", x = "Components", y = "Cumulative Variance") +
+  theme_minimal()
+
+# Add horizontal and vertical lines to Plot 5
+plt5 <- plt5 + geom_hline(yintercept = 0.8, linetype = "dashed") +
+  geom_vline(xintercept = which(floor(cumsum(EVD$values) / sum(EVD$values) * 10) == 8)[1], linetype = "dashed")
+
+# Combine the two plots
+plots <- grid.arrange(plt1, plt2, plt3, plt4, plt5)
+ggsave("../../output/e_Eigen.pdf", plots)
