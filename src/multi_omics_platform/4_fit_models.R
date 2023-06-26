3ab1cea8138b958994de9f879fa9fc3910a05d4a
frankiejgit <frankiejgram@gmail.com>
2023-06-26 15:29:55 -0400

    creating BGLR function

--- src/multi_omics_platform/4_fit_models.R
+++ src/multi_omics_platform/4_fit_models.R
@@ -1,91 +1,15 @@
-# Import packages
-library(data.table)
-library(ggplot2)
-library(gridExtra)
-library(BGLR)
+runBGLR <- function(phenos, col.phen, col.var, col.cv, col.env = NULL,folds = 5,
+                    cv0 = FALSE, esc=FALSE, nIter = 5000, burnIn = 500) {
+  
+  # Get values for model fitting
+  phen.name <- strsplit(colnames(phenos)[col.phen], "_")[[1]][1]
+  y <- phenos[, col.phen]
+  gid <- phenos[, col.var]
+  
+  if (esc) { y <- scale()}
+  
+}
 
-# Import modules
-source("1_data_load.R")
-source("2_matrices.R")
-source("3_cv_prep.R")
-
-### USER ARGUMENTS ###
-
-args <- commandArgs(trailingOnly = TRUE)
-nan.freq <- 0.2                            # NaN threshold frequency
-col.env.id <- 11                           # Env ID in phenos file
-marker.path <- "../../data/SNPs.rda"       # Covariate matrix file
-phenos.path <- "../../data/Phenos.csv"     # Phenotype/Environment file
-
-ctr <- TRUE
-std <- TRUE
-weighting <- FALSE
-prop.maf.j <- NULL 
-
-cv.reps <- 5
-trait.cols <- 3:11
-
-### 1 - Data Load ###
-loaded.data <- loadData(phenos.path, marker.path)
-
-markers <- loaded.data$markers
-phenos <- loaded.data$phenos
-
-rm(loaded.data)
-
-# Create the NaNs csv files
-createNaNFiles(phenos, markers, nan.freq)  # These files are the Mod1 outputs
-
-### 2 - G/E Matrices ###
-
-# E matrix
-generateMatrix("../../output/E/", phenos, markers = NULL, col.env.id = col.env.id )
-# G matrix
-generateMatrix("../../output/G/", phenos=phenos, markers=markers, 
-                col.env.id = 2, prop.maf.j =  NULL)
-# ZE matrix
-createZMatrix(phenos, 11, "../../output/ZE/")
-# ZL matrix
-createZMatrix(phenos, 2, "../../output/ZL/") # TODO Confirm what dataset to use
-
-# Interaction matrix
-g1.file <- '../../output/G/G.rda'          # path to matrix file 1
-g2.file <- '../../output/E/G.rda'          # path to matrix file 2
-
-generateIntMatrix(g1.file, g2.file, output.path='../../output/GE/')
-
-### 3 - Phenotype data prep ###
-set.seed(1)
-
-# Do only CV1 and CV2
-phenos.cv <- cvPrep(phenos, paste0("../../output/"), col.id=2, folds = 5, cv0 = FALSE, cv00 = FALSE)
-
-# Do only CV0 and CV00
-for (col in trait.cols) {
-  col.name <- colnames(phenos.cv)[col]
-  phenos.cv <- cvPrep(phenos.cv, "../../output/cv/", col.id = col, col.folds = 14,
-                      folds = 5, cv1 = FALSE, cv2 = FALSE)
-  }
-
-### 4 - Fit models ####
-
-# Output structure: trait (e.g height) --> CV --> fold_n --> predictions.csv 
-
-# CV0 - E+L
-# Only thing that changes between folds is col.phen
-
-nIter <- 12000
-burnIn <- 2000
-folds <- 6
-reps <- 10
-
-col.phen <- 15 # starting CV0 results
-col.var <- 2  # ID of variety
-col.cv <- 12
-phen.folds <- 1:5
-col.env <- NULL
-cv0 <- TRUE
-esc <- FALSE
 
 phen.name <- strsplit(colnames(phenos.cv)[col.phen], "_")[[1]][1]
 y   <- phenos.cv[, col.phen]
@@ -124,7 +48,7 @@ for (fold in 1:folds) {
     
     predictions <- data.frame(testing = testing, Individual = gid[testing], y = y[testing], yHat = fm$yHat[testing])
     write.table(predictions, file = paste(output.path, "predictions_", fold, ".csv", sep=''), row.names = FALSE, sep = ",")
-
+    
   } else {
     output.path <- paste("../../output/", phen.name, "/full_data/", sep='')
     if (!dir.exists(output.path)) { dir.create(output.path) }
@@ -197,3 +121,121 @@ for (fold in 1:folds) {
   rm(fm)
   file.remove(list.files(pattern = "*.dat"))
 }
+
+
+
+
+
+nk <-length(AB)
+ETA<-list(nk)
+for(i in 1:nk){
+  
+  if(type[i]=='BRR')
+  {
+    load(AB[[i]])
+    ETA[[i]] <- list(X=Z,model='BRR')
+    rm(Z)
+  }
+  
+  if(type[i]=='FIXED')
+  {
+    load(AB[[i]])
+    ETA[[i]] <- list(X=Z,model='FIXED')
+    rm(Z)
+  }
+  
+  if(type[i]=='RKHS')
+  {
+    load(AB[[i]])
+    ETA[[i]] <- list(V=EVD$vectors,d=EVD$values,model='RKHS')
+    rm(EVD)
+  }
+  
+  if(type[i]=='BayesA')
+  {
+    load(AB[[i]])
+    ETA[[i]] <- list(X=X,model='BayesA')
+    rm(X)
+  }
+  
+  if(type[i]=='BayesB')
+  {
+    load(AB[[i]])
+    ETA[[i]] <- list(X=X,model='BayesB')
+    rm(X)
+  }
+  
+  if(type[i]=='BayesC')
+  {
+    load(AB[[i]])
+    ETA[[i]] <- list(X=X,model='BayesC')
+    rm(X)
+  }
+  
+  if(type[i]=='BL')
+  {
+    load(AB[[i]])
+    ETA[[i]] <- list(X=X,model='BL')
+    rm(X)
+  }  
+  print(i)
+}
+
+
+for(fold in folds)
+{
+  
+  yNA<-y
+  print(fold)
+  
+  if(fold != -999)
+  {
+    
+    dir.create(paste('fold_',fold,sep=''))
+    setwd(paste('fold_',fold,sep=''))
+    
+    testing=which(Y[,colCV]==fold)
+    
+    if(CV0)
+    {         
+      testing <- which(gid %in% gid[testing])   
+    }  
+    
+    
+    yNA=y
+    yNA[testing]=NA
+    
+    
+    fm=BGLR(y=yNA,ETA=ETA,nIter=nIter,burnIn=burnIn,verbose=TRUE)
+    fm$y=y
+    
+    predictions=data.frame(testing,Individual=gid[testing], y=y[testing], yHat=fm$yHat[testing])
+    
+    write.table(predictions,file=paste("predictions_",fold,".csv",sep=""),row.names=FALSE,sep=",") # Change to a unique name?
+    
+  }else{
+    
+    dir.create('fullData')
+    setwd('fullData')
+    
+    fm=BGLR(y=y,ETA=ETA,nIter=nIter,burnIn=burnIn,verbose=TRUE)
+    save(fm,file='fm_full.RData')
+    
+  }          
+  
+  print(str(fm))
+  rm(fm)
+  
+  
+  unlink("*.dat")
+  
+  setwd('..')
+  
+}
+
+
+
+
+
+
+
